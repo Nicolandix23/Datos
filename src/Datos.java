dcf67d7c08d0a224eadaf8613f3860f14a855333
nicolandix23 <nikolas.rojas.rubio@gmail.com>
2022-05-15 20:57:18 -0500

    añadimos datos

---
+++ src/Datos.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class Datos {
+
+	public static void main(String[] args) {
+	
+     
+		
+		int numero1 = 7;
+		int numero2 = 3;
+		
+		int respuesta_suma = numero1 + numero2;
+		int respuesta_resta = numero1 - numero2;
+		int respuesta_multiplicacion = numero1 * numero2;
+		int respuesta_division = numero1 / numero2;
+		int respuesta_elevacion = numero1 * numero1 * numero1;
+		
+       System.out.println("la suma es \n " + respuesta_suma );
+       
+       System.out.println("la resta es \n " + respuesta_resta );
+       
+       System.out.println("la multiplicacion es \n " + respuesta_multiplicacion );
+	
+       System.out.println("la division es \n " + respuesta_division );
+       
+       System.out.println("la elevacion es \n " + respuesta_elevacion );
+	}
+
+}
